62d1aed86321ed132c3e4ab4024b171af16da363
Guilherme Lourenço de Miranda <guilherme.miranda325@gmail.com>
2019-05-15 17:26:18 -0300

    Arquivos novos

---
+++ include/exemplo02.php
@@ -0,0 +1,11 @@
+<?php
+
+	// include "inc/exemplo01.php";
+	require_once "inc/exemplo01.php";
+	require_once "inc/exemplo01.php";
+
+	$resultado = somar(10, 25);
+
+	echo $resultado;
+
+?>
---
+++ strings/exemplo01.php
@@ -0,0 +1,11 @@
+<?php
+
+	$nome = "HCode";
+
+	$nome2 = 'Treinamentos';
+
+	//var_dump($nome, $nome2);
+
+	echo 'ABC $nome';
+
+?>
