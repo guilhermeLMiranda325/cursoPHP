ab866ad494557e9c851f12ccd404e6546ab04384
Guilherme Lourenço de Miranda <guilherme.miranda325@gmail.com>
2019-06-10 08:45:12 -0300

    Aula de autoload.

---
+++ autoload/Abstratas/Automovel.php
@@ -0,0 +1,37 @@
+<?php
+
+	interface Veiculo
+	{
+
+		public function acelerar($velocidade);
+		public function frenar($velocidade);
+		public function trocarMarcha($marcha);
+
+	}
+
+	class Automovel implements Veiculo
+	{
+		
+		public function acelerar($velocidade)
+		{
+			
+			echo "O vaículo acelerou até " . $velocidade . " km/h";
+
+		}
+
+		public function frenar($velocidade)
+		{
+
+			echo "O veículo frenou até " . $velocidade . " km/h";
+
+		}
+
+		public function trocarMarcha($marcha)
+		{
+
+			echo "O veículo engatou a marcha " . $marcha;
+
+		}
+	}
+
+?>
