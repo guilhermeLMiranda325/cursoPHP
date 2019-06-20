3717845d0feb21b672831de9b872177e806dbeb1
Guilherme Lourenço de Miranda <guilherme.miranda325@gmail.com>
2019-06-20 16:59:06 -0300

    Lista DAO-PDO

--- DAO/index.php
+++ DAO/index.php
@@ -11,10 +11,33 @@
 	echo json_encode($usuarios);
 	*/
 
+	/*
+	Carrega um usuário
 	$root = new Usuario();
 
-	$root->loadById(1);
+	$root->loadById(2);
 
 	echo $root;
+	*/
+
+	/*
+	Carrega uma lista de usuários
+	$lista = Usuario::getList();
+
+	echo json_encode($lista);
+	*/
+
+	/*
+	Carrega uma lista de usuários buscando pelo login
+	$search = Usuario::search("jo");
+
+	echo json_encode($search);
+	*/
+
+	//Carrega um usuário usando login e a senha
+	$usuario = new Usuario();
+	$usuario->login("root", "!@#$");
+
+	echo $usuario;
 
 ?>
